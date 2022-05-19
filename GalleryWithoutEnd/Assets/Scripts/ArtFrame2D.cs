16d891cf041a9d9e69d60bbdc7f97d4274a510c7
Maxwell Lovell <maxwellloganlovell@gmail.com>
2022-05-19 16:52:56 -0400

    [Fix] Corrected picture frame scaling so it changes shape depending on the aspect ratio

--- GalleryWithoutEnd/Assets/Scripts/ArtFrame2D.cs
+++ GalleryWithoutEnd/Assets/Scripts/ArtFrame2D.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-public class ArtFrame : MonoBehaviour
+public class ArtFrame2D : MonoBehaviour
 {
     [SerializeField] private GameObject ArtManager;
     private IApiManager ArtManagerInterface;
 
-    [SerializeField] private Image artwork;
     [SerializeField] private AspectRatioFitter aspectFitter;
+    [SerializeField] private RectTransform frame;
+
+    [SerializeField] private Image artwork;
 
     [SerializeField] private TextMeshProUGUI title;
     [SerializeField] private TextMeshProUGUI artist;
@@ -39,13 +41,29 @@ public class ArtFrame : MonoBehaviour
     void UpdateUI()
     {
         // Setup and scale artwork
+
         artwork.enabled = false;
+
         var rect = new Rect(0, 0, ArtPiece.Art.width, ArtPiece.Art.height);
         artwork.sprite = Sprite.Create(ArtPiece.Art, rect, artwork.transform.position);
+
         artwork.type = Image.Type.Simple;
         artwork.preserveAspect = true;
+
         Debug.Log($"Art Size: {ArtPiece.Art.width} by {ArtPiece.Art.height}");
-        aspectFitter.aspectRatio = (float)ArtPiece.Art.width / ArtPiece.Art.height;
+
+        float aspectRatio = (float)ArtPiece.Art.width / ArtPiece.Art.height;
+        aspectFitter.aspectRatio = aspectRatio;
+
+        aspectRatio = Mathf.Round(aspectRatio * 100.0f) * 0.01f;
+
+        if (aspectRatio > 1)
+            frame.sizeDelta = new Vector2(550, 420);
+        else if (aspectRatio < 1)
+            frame.sizeDelta = new Vector2(350, 420);
+        else
+            frame.sizeDelta = new Vector2(420, 420);
+
         artwork.enabled = true;
 
         title.text = ArtPiece.Title;
